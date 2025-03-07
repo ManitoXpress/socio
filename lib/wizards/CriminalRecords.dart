d4104b7bed1a6ee5376655c3ad79702373d5d6b5
M1X3R <107292117+m1x3r961@users.noreply.github.com>
2025-03-07 15:24:16 -0400

    agree pdf certificate
    modifique para agregar el pdf junto ccon la imagen

--- lib/wizards/CriminalRecords.dart
+++ lib/wizards/CriminalRecords.dart
@@ -1,19 +1,12 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'package:camera/camera.dart';
 import 'dart:io';
 
 import 'package:socio/Metods/RegisController.dart';
-import 'package:socio/ServiceResponse/request.dart';
-import 'package:socio/ServiceResponse/requestUserData.dart';
-import 'package:socio/Utils/styles.dart';
 
-import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:image_picker/image_picker.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:socio/ServiceResponse/requestUserData.dart';
 
 
 class CriminalRecordImageStep extends StatefulWidget {
@@ -45,80 +38,130 @@ class _CriminalRecordImageStepState extends State<CriminalRecordImageStep> {
   final ImagePicker _imagePicker = ImagePicker();
 
   // Función para seleccionar o capturar imagen
-  Future<void> _pickImage() async {
-  try {
-    // Mostrar cuadro de diálogo para tomar una foto
-    await showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Tome una foto a su antecedentes penales'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  Navigator.pop(context); // Cierra el cuadro de diálogo
-                  try {
-                    // Intentar capturar una imagen desde la cámara
-                    final XFile? image = await _imagePicker.pickImage(
-                      source: ImageSource.camera,
-                    );
-                    if (image != null) {
-                      print('Imagen capturada: ${image.path}');
-                      _processImage(image); // Procesa la imagen capturada
-                    } else {
-                      print('No se capturó ninguna imagen.');
-                    }
-                  } catch (e) {
-                    print('Error al acceder a la cámara: $e');
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(
-                          'No se pudo acceder a la cámara. Por favor, verifica los permisos en la configuración del dispositivo.',
+  Future<void> _pickFile() async {
+    try {
+      await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Seleccione una opción'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    try {
+                      final XFile? image = await _imagePicker.pickImage(
+                        source: ImageSource.camera,
+                      );
+                      if (image != null) {
+                        print('Imagen capturada: ${image.path}');
+                        _processFile(image.path);
+                      } else {
+                        print('No se capturó ninguna imagen.');
+                      }
+                    } catch (e) {
+                      print('Error al acceder a la cámara: $e');
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'No se pudo acceder a la cámara. Por favor, verifica los permisos en la configuración del dispositivo.',
+                          ),
                         ),
-                      ),
-                    );
-                  }
-                },
-                child: const Text('Tomar Foto'),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  } catch (e) {
-    print('Error al mostrar el cuadro de diálogo: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Ocurrió un error: $e'),
-      ),
-    );
-  }
-}
-
-
-  // Procesa la imagen seleccionada o capturada
-  void _processImage(XFile? image) {
-    if (image != null) {
-      setState(() {
-        _image = File(image.path); // Guarda la imagen seleccionada
-      });
-
-      // Llama al callback para notificar la selección de la ruta de la imagen
-      widget.onImageSelected(image.path);
-
-      // Actualiza el estado en RegistrationController (si es necesario)
-      widget.registrationController.updateRegistrationData(
-        idDocumentImagePath: '',
-        workerType: '',
-        idDocumentImagePath2:'',
-        certificateImagePaths:  '', criminalRecordImagePath: image.path,
+                      );
+                    }
+                  },
+                  child: const Text('Tomar Foto'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    try {
+                      final XFile? image = await _imagePicker.pickImage(
+                        source: ImageSource.gallery,
+                      );
+                      if (image != null) {
+                        print('Imagen seleccionada: ${image.path}');
+                        _processFile(image.path);
+                      } else {
+                        print('No se seleccionó ninguna imagen.');
+                      }
+                    } catch (e) {
+                      print('Error al acceder a la galería: $e');
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'No se pudo acceder a la galería. Por favor, verifica los permisos en la configuración del dispositivo.',
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Seleccionar de Galería'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    try {
+                      final FilePickerResult? result = await FilePicker.platform.pickFiles(
+                        type: FileType.custom,
+                        allowedExtensions: ['pdf'],
+                      );
+                      if (result != null) {
+                        final String? filePath = result.files.single.path;
+                        if (filePath != null) {
+                          print('Documento PDF seleccionado: $filePath');
+                          _processFile(filePath);
+                        }
+                      } else {
+                        print('No se seleccionó ningún documento.');
+                      }
+                    } catch (e) {
+                      print('Error al seleccionar el documento: $e');
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'No se pudo seleccionar el documento. Por favor, inténtalo de nuevo.',
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Seleccionar PDF'),
+                ),
+              ],
+            ),
+          );
+        },
       );
-
-      print('Imagen de documento seleccionada: ${image.path}');
+    } catch (e) {
+      print('Error al mostrar el cuadro de diálogo: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Ocurrió un error: $e'),
+        ),
+      );
     }
+  }
+
+  // Procesa el archivo seleccionado
+  void _processFile(String filePath) {
+    setState(() {
+      _image = File(filePath);
+    });
+
+    widget.onImageSelected(filePath);
+
+    widget.registrationController.updateRegistrationData(
+      idDocumentImagePath: '',
+      workerType: '',
+      idDocumentImagePath2: '',
+      certificateImagePaths: filePath,
+      criminalRecordImagePath: '',
+    );
+
+    print('Archivo seleccionado: $filePath');
   }
 
   @override
@@ -128,12 +171,12 @@ class _CriminalRecordImageStepState extends State<CriminalRecordImageStep> {
         const Padding(
           padding: EdgeInsets.symmetric(vertical: 16),
           child: Text(
-            "Paso 7: Necesitamos una foto de sus antecedentes penales",
+            "Paso 7: Necesitamos una foto de sus certificados profesionales o un documento PDF (no obligatorio)",
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ),
         GestureDetector(
-          onTap: _pickImage,
+          onTap: _pickFile,
           child: Container(
             width: 200,
             height: 200,
@@ -145,6 +188,14 @@ class _CriminalRecordImageStepState extends State<CriminalRecordImageStep> {
                 ? const Center(
               child: Icon(
                 Icons.cloud_upload,
+                size: 48,
+                color: Color(0xA3C9D2D2),
+              ),
+            )
+                : _image!.path.endsWith('.pdf')
+                ? const Center(
+              child: Icon(
+                Icons.picture_as_pdf,
                 size: 48,
                 color: Color(0xA3C9D2D2),
               ),
@@ -161,7 +212,7 @@ class _CriminalRecordImageStepState extends State<CriminalRecordImageStep> {
           const Padding(
             padding: EdgeInsets.all(8.0),
             child: Text(
-              'Saca una foto antes de continuar.',
+              'Saca una foto, selecciona una imagen o un documento PDF antes de continuar.',
               style: TextStyle(color: Color(0xFF830A09)),
             ),
           ),
